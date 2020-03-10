492537a98ea19ca6a909561b3490bded9c86b766
cryptonaut420 <nrathman@ironcladtech.ca>
2020-03-10 15:34:14 -0700

    update debug to console.error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // dotenv at CONFIG_FILE or .env
 let dotenv = require('dotenv')
-require('dotenv').load({path: process.env.CONFIG_FILE ? process.env.CONFIG_FILE : '.env'})
+require('dotenv').config()
 
 let TESTNET = (process.env.TESTNET === '1' || (process.env.TESTNET && process.env.TESTNET.toLowerCase() === 'true'))
 let REGTEST = (process.env.REGTEST === '1' || (process.env.REGTEST && process.env.REGTEST.toLowerCase() === 'true'))
@@ -9,9 +9,9 @@ if (TESTNET && REGTEST) {
   throw new Error('Cannot specify REGTEST and TESTNET at the same time')
 }
 
+
 let debug = require('debug')('index')
 let express = require('express')
-
 let service = require('./lib/service')
 let api = require('./lib/express')
 
@@ -25,15 +25,15 @@ if (process.env.CORS === '*') {
 }
 
 // run the service
-debug(`Initializing blockchain connection${["", " (for testnet)", " (for regtest)"][(TESTNET?1:0)+(REGTEST?2:0)]}`)
+console.error(`Initializing blockchain connection${["", " (for testnet)", " (for regtest)"][(TESTNET?1:0)+(REGTEST?2:0)]}`)
 service((err, adapter) => {
   if (err) {
-    return debug('Initialization failed:', err)
+    return console.error('Initialization failed:', err)
   }
 
   // start the API server
-  debug('starting API server')
+  console.error('starting API server')
   app.use(api(adapter, {testnet: TESTNET, regtest: REGTEST, custom: process.env.CUSTOM}))
   app.listen(process.env.SERVER_PORT);
-  debug("App listening on port "+process.env.SERVER_PORT);
+  console.error("App listening on port "+process.env.SERVER_PORT);
 })
